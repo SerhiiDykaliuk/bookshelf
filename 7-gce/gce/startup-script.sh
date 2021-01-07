851898afb00dcd65b16caaeec57a091e744437ba
SerhiiDykaliuk <breeck.ua@gmail.com>
2021-01-07 16:42:30 +0200

    final with git clone from gcp mirrored

--- 7-gce/gce/startup-script.sh
+++ 7-gce/gce/startup-script.sh
@@ -32,8 +32,11 @@ pip install --upgrade pip virtualenv
 # Get the source code from the Google Cloud Repository
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
+#echo $PROJECTID
+#gcloud source repos clone github_serhiidykaliuk_bookshelf /opt/app --project=manual-python-bookshelf
 git config --global credential.helper gcloud.sh
-git clone https://source.developers.google.com/p/$PROJECTID/r/github_serhiidykaliuk_bookshelf/+/main /opt/app
+git clone https://source.developers.google.com/p/$PROJECTID/r/github_serhiidykaliuk_bookshelf /opt/app
+(cd /opt/app && git checkout main)
 
 # Install app dependencies
 virtualenv -p python3 /opt/app/7-gce/env
