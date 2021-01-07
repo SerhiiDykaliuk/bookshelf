c9517b8db8030b0e4f30f4326cc190769d15d3ea
SerhiiDykaliuk <breeck.ua@gmail.com>
2021-01-07 12:39:32 +0200

    change SCM to cloud source; added sql proxy

--- 7-gce/gce/startup-script.sh
+++ 7-gce/gce/startup-script.sh
@@ -18,6 +18,11 @@ apt-get install -yq \
     git build-essential supervisor python python-dev python-pip libffi-dev \
     libssl-dev
 
+# Install CloudSQL Proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+./cloud_sql_proxy -instances=manual-python-bookshelf:europe-west3:bookshelf-db=tcp:3306 -ip_address_types=PRIVATE
+
 # Create a pythonapp user. The application will run as this user.
 useradd -m -d /home/pythonapp pythonapp
 
@@ -27,8 +32,8 @@ pip install --upgrade pip virtualenv
 # Get the source code from the Google Cloud Repository
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
-# git config --global credential.helper gcloud.sh
-git clone https://github.com/SerhiiDykaliuk/bookshelf.git /opt/app
+git config --global credential.helper gcloud.sh
+git clone https://source.developers.google.com/p/$PROJECTID/r/github_serhiidykaliuk_bookshelf /opt/app
 
 # Install app dependencies
 virtualenv -p python3 /opt/app/7-gce/env
