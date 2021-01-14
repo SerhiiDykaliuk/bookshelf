afa2d1589e5b7229a36f33d98298272ce225c9cb
SerhiiDykaliuk <breeck.ua@gmail.com>
2021-01-14 21:06:50 +0200

    instance test

--- 7-gce/config.py
+++ 7-gce/config.py
@@ -32,7 +32,7 @@ DATA_BACKEND = 'cloudsql'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'manual-python-bookshelf'
+PROJECT_ID = 'tf-bookshelf-project'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
@@ -90,7 +90,7 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'my-bookshelf-sample-app-bucket'
+CLOUD_STORAGE_BUCKET = 'tf-image-storage-bucket'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
